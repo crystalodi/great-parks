257e74641b8ee00b21982dfec4de6790327f328f
Crystal Odi <crystalodi@gmail.com>
2021-08-30 02:44:15 -0500

    finish footer and logo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <header>
-    <div id="logo">
-      <div class="container">
-        <div class="logo-content">
-          <h4>Logo!</h4>
-        </div>
-      </div>
-    </div>
-    <div id="hero">
-      <div class="container">
-        <div class="hero-content">
-          <h1>Great Parks of California</h1>
-        </div>
-      </div>
+  <header id="header">
+    <h1 class="logo">The Great Parks Project <span class="logo-icon"><i class="fas fa-leaf"></i></span></h1>
+  </header>
+  <header id="hero">
+    <div class="hero-content">
+      <h1 class="hero-title">Explore California Beauty</h1>
+      <p class="hero-lead">Venture into the great outdoors and see the natural sights of California</p>
     </div>
   </header>
   <section id="parks">
       <Card v-for="park of parks" :key="park.id" :park="park"/>
   </section>
+  <footer id="footer">
+    <div class="footer-sitename">
+      <h3>The Great Parks Project <span class="logo-icon"><i class="fas fa-leaf"></i></span></h3>
+              <p>Copyright &copy; 2021, All Rights Reserved</p>
+        <a href="#" class="footer-link">Privacy Policy</a> <a href="#" class="footer-link">Terms and Conditions</a>
+    </div>
+    <div class="footer-socialmedia">
+      <a href="https://www.twitter.com/natlparkservice" target="_blank"><i class="fab fa-twitter fa-3x"></i></a>
+    </div>
+  </footer>
 </template>
 
 <script>
